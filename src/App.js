dd3556fdc77ad32a487df80becd76b206a1ed475
Cassandra Villarreal <cass2u94@gmail.com>
2020-07-19 19:50:30 -0400

    Components added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Places from "./Components/Places"
+import PizzaForm from "./Components/PizzaForm"
 
 const App = () => {
+
   return (
-    <>
+    <div className = "Home">
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <p>It is always time for pizza</p>
+    </div>
   );
 };
 export default App;
